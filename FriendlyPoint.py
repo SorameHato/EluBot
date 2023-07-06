cb1f6cf78e7266b7c923119be7bea28954741b63
Jeon Hojin <ghwls030306@naver.com>
2023-07-06 17:06:53 +0900

    코드 중복 해결
    아무래도 데이터 구조 잘못 짰나.. 지금이라도 last_call을 date형으로 바꿀까

--- FriendlyPoint.py
+++ FriendlyPoint.py
@@ -178,25 +178,23 @@ def __addData__(sql_cur, uid:int, data_name:str, amount):
     '''
     sql_cur.execute(f'UPDATE friendly_point SET {data_name}(SELECT {data_name} FROM friendly_point WHERE uid={uid})+{amount} WHERE uid={uid}')
 
-def getCommandCount(uid:int):
+def __getDataFromOutside__(uid:int, attribute:str):
+    '''코드가 비슷한 것 같아서 그냥 4개를 전부 합쳐버림'''
     sql_con, sql_cur = __connectDB__()
-    command_count = __getData__(sql_cur,'command_count',uid)
-    __logWrite__(uid,'조회',f'command_count={command_count}')
+    result = __getData__(sql_cur,attribute,uid)
+    __logWrite__(uid,'조회',f'{attribute}={result}')
     __closeCon__(sql_con)
-    return command_count
+    return result
+
+def getCommandCount(uid:int):
+    return __getDataFromOutside__(uid, 'command_count')
 
 def getDayCount(uid:int):
-    sql_con, sql_cur = __connectDB__()
-    day_count = __getData__(sql_cur,'day_count',uid)
-    __logWrite__(uid,'조회',f'day_count={day_count}')
-    __closeCon__(sql_con)
-    return day_count
+    return __getDataFromOutside__(uid, 'day_count')
 
 def getPenalty(uid:int):
-    sql_con, sql_cur = __connectDB__()
-    total_penalty = __getData__(sql_cur,'total_penalty',uid)
-    __logWrite__(uid,'조회',f'total_penalty={total_penalty}')
-    __closeCon__(sql_con)
-    return total_penalty
+    return __getDataFromOutside__(uid, 'total_penalty')
 
-def 
+def getLastCallDate(uid:int):
+    return __getDataFromOutside__(uid, 'last_call')
+    #str형으로 반환, 내부에서 작업할 때는 %Y-%m-%d %H:%M:%S.%f 형식으로 datetime형으로 변환해야 함
