06a689f4fa0e78c3096cf03ad6225f50fed05f03
Jeon Hojin <ghwls030306@naver.com>
2023-07-12 00:26:50 +0900

    fix : 바보토끼 수정

--- FriendlyRate.py
+++ FriendlyRate.py
@@ -323,9 +323,9 @@ def __updateLastCallDate__(sql_con, sql_cur, uid:int, date:dt, sep=False):
     now = dt.now(tz(td(hours=9)))
     __setData__(sql_con, sql_cur,uid,'last_call',now)
     if now.time() >= time(5,15):
-        todayStart = dt(now.year, now.month, now.day, 5, 15)
+        todayStart = dt(now.year, now.month, now.day, 5, 15, tzinfo=tz(td(hours=9)))
     else:
-        todayStart = dt(now.year, now.month, now.day, 5, 15)-td(days=1)
+        todayStart = dt(now.year, now.month, now.day, 5, 15, tzinfo=tz(td(hours=9)))-td(days=1)
     if last_call < todayStart:
         #왜 abs냐면 음수로 나와서
         #In [64]: last_call = dt.strptime('2023-07-08 05:14:59.000','%Y-%m-%d %H:%M:%S.%f')
