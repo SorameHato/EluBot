efebbc7a2059467f68ef34b1ba0b7aea03908397
Jeon Hojin <ghwls030306@naver.com>
2023-08-13 23:45:15 +0900

    change : friendlyRate 코드 변경
    connectDB 밑에서 안 쓰도록 변경
    여러 개 한꺼번에 get해올 수 있게 수정
    전체 친밀도 계산 기능 추가 등

--- FriendlyRate.py
+++ FriendlyRate.py
@@ -8,9 +8,8 @@ from SkyLib import tui
 getcontext().prec = 3
 commandPoint = Decimal('0.15')
 dayPoint = Decimal('2.85')
-threeDayPenalty = Decimal('3')
-sevenDayPenaltyInitial = Decimal('2')
-sevenDayPenaltyPerDay = Decimal('14')
+sevenDayPenaltyInitial = Decimal('28')
+sevenDayPenaltyPerDay = Decimal('2')
 
 '''
 에루짱의 친밀도 정리
@@ -23,11 +22,12 @@ sevenDayPenaltyPerDay = Decimal('14')
 테이블 구조 : uid INTEGER, last_call TEXT, gunba BLOB, command_count INTEGER, day_count INTEGER, total_penalty REAL, friendly_rate REAL
 명령어 사용 한 번 : +0.15 (command_count 행)
 매일 최초 명령어 사용(05:15를 기준으로 초기화) : 추가 +2.85 (day_count 행)
-3일 이상 에루봇을 사용하지 않다가 사용 : 포인트 -3 감소 및 에루짱이 외로워하는 대사 출력
-다만 군바나 학업 등으로 자주 디코에 접속하지 못 하는 상황에서는 대사만 출력하고 포인트는 깎지 않음
-(gunba, 그치만 8일 이상 미접속은 군바라도 예외 없이 깎음)
-일주일 이상 에루봇을 사용하지 않다가 사용 (8일차부터) : -14 (+ 이후 1일 당 -2 추가) + 에루짱이 삐진 듯 한 대사 출력
-그러니까 8일 : -11, 9일 : -13, 10일 : -15, 11일 : -17 ...
+3일 이상 에루봇을 사용하지 않다가 사용 : 에루짱이 외로워하는 대사 출력
+1주일 이상 에루봇을 사용하지 않다가 사용 : -3 + 에루짱이 외로워하는 대사 출력
+(외로워하는 대사는 3~6일, 7~14일 각각 다르게)
+2주일 이상 에루봇을 사용하지 않다가 사용 (15일차부터) : -28 (+ 이후 1일 당 -2 추가) + 에루짱이 삐진 듯 한 대사 출력
+그러니까 15일 : -28, 16일 : -30, 17일 : -32, 18일 : -34 ...
+다만 군바나 학업 등으로 자주 디코에 접속하지 못 하는 상황에서는 대사만 출력하고 포인트는 깎지 않음 (gunba)
 
 main.py에서 슬래시커맨드를 사용하면 정보나 랭크 커맨드 같은 게 아닌 이상
 무조건적으로 event를 발생시켜서 commandCallCalc 함수를 호출
@@ -178,19 +178,41 @@ def register(uid:int,gunba:int=0):
         __closeCon__(sql_con)
         return -1
     
-
-def __getData__(sql_cur, uid:int, data_name:str, outside=False):
+def __getData__(uid:int, data_name:str, outside=False):
     '''
     friendly_rate 테이블에서 uid에 대한 data_name의 값을 가지고 오는 함수
     '''
-    if data_name in ['first_call', 'last_call', 'gunba', 'command_count', 'day_count', 'total_penalty', 'friendly_rate'] and type(uid) is int:
-        sql_cur.execute(f'SELECT {data_name} FROM friendly_rate WHERE uid=:uid;',{'uid':uid})
-        sql_data = sql_cur.fetchall()
-        result = sql_data[0][0]
-        if outside:
-            __logWrite__(uid,'조회(외부)',f'{data_name}={result}')
+    if data_name in ['uid', 'first_call', 'last_call', 'gunba', 'command_count', 'day_count', 'total_penalty', 'friendly_rate', '*'] and (type(uid) is int or uid is None):
+        sql_con, sql_cur = __connectDB__()
+        if uid is None:
+            sql_cur.execute(f'SELECT {data_name} FROM friendly_rate;')
+            dType = 2
         else:
-            __logWrite__(uid,'조회(내부)',f'{data_name}={result}')
+            sql_cur.execute(f'SELECT {data_name} FROM friendly_rate WHERE uid=:uid;',{'uid':uid})
+            dType = 0
+        sql_data = sql_cur.fetchall()
+        if data_name == '*':
+            dType += 1
+        match dType:
+            case 0:
+                result = sql_data[0][0]
+            case 1:
+                result = sql_data[0]
+            case 2:
+                result = []
+                for item in data:
+                    result.append(item[0])
+            case 3:
+                result = sql_data
+        if outside:
+            com = '조회(외부)'
+        else:
+            com = '조회(내부)'
+        if dType < 2:
+            __logWrite__(uid,com,f'{data_name}={result}')
+        else:
+            __logWrite__(uid,com,f'{data_name}을 전체 유저에 걸쳐서 조회')
+        __closeCon__(sql_con)
         return result
     else:
         return 0
@@ -211,36 +233,35 @@ def __dataCheck__(uid, data_name, amount, funcInfo):
     elif funcInfo == 'set':
         dataList = ['last_call', 'gunba', 'command_count', 'day_count', 'total_penalty', 'friendly_rate']
     else:
-        raise Exception(f'dataCheck 함수에서 코드 종류가 잘못 지정되었습니다. add 또는 set이 지정되어야 하는데 {funcInfo}가 지정되었습니다.')
+        raise ValueError(f'dataCheck 함수에서 코드 종류가 잘못 지정되었습니다. add 또는 set이 지정되어야 하는데 {funcInfo}가 지정되었습니다.')
     
     try:
         # dataList의 이름 체크
         if data_name not in dataList:
-            raise Exception(f'__addData__에 지정된 attribute 이름이 잘못되었습니다. 지정된 attribute 이름은 {data_name}입니다.')
+            raise ValueError(f'__{funcInfo}Data__에 지정된 attribute 이름이 잘못되었습니다. 지정된 attribute 이름은 {data_name}입니다.')
         # uid가 int인지 체크
         if type(uid) != int:
-            raise ValueError(f'uid의 타입이 잘못되었습니다. uid는 int형이여야 합니다. uid의 타입 : {type(uid)}')
+            raise TypeError(f'uid의 타입이 잘못되었습니다. uid는 int형이여야 합니다. uid의 타입 : {type(uid)}')
         # data_name이 각 형식 별 형식에 맞는지 체크
-        # total_penalty, friendly_rate : int형이나 Decimal형
-        # gunba : bool형
+        # total_penalty, friendly_rate : int형이나 float형 (SQLite에 UPDATE를 할 때는 float형이여야 함, 그 이후에 Decimal로 변경)
         # last_call : dt형
-        # 그 외(command_count, day_count) : int형
+        # 그 외(command_count, day_count, gunba 등) : int형
         if data_name in ['total_penalty','friendly_rate']:
             if type(amount) != int and type(amount) != float:
-                raise ValueError(f'amount의 타입이 잘못되었습니다. amount는 int형 또는 float형이여야 합니다. amount의 타입 : {type(amount)}')
+                raise TypeError(f'amount의 타입이 잘못되었습니다. amount는 int형 또는 float형이여야 합니다. amount의 타입 : {type(amount)}')
         elif data_name == 'last_call':
             if type(amount) != dt and type(amount) != str:
-                raise ValueError(f'amount의 타입이 잘못되었습니다. amount는 datetime형이여야 합니다. amount의 타입 : {type(amount)}')
+                raise TypeError(f'amount의 타입이 잘못되었습니다. amount는 datetime형이여야 합니다. amount의 타입 : {type(amount)}')
         else:
             if type(amount) != int:
-                raise ValueError(f'amount의 타입이 잘못되었습니다. amount는 int형이여야 합니다. amount의 타입 : {type(amount)}')
+                raise TypeError(f'amount의 타입이 잘못되었습니다. amount는 int형이여야 합니다. amount의 타입 : {type(amount)}')
     except Exception as exceptA:
         raise exceptA
         return False
     else:
         return True
 
-def __setData__(sql_con, sql_cur, uid:int, data_name:str, amount, sep=False):
+def __setData__(uid:int, data_name:str, amount, sep=False):
     '''
     데이터 수동 수정 용으로 만든 함수임!
     군바나 last_call, friendly_rate 설정하는 경우가 아니라면 절대로 함수 안에서 사용하지 말 것!
@@ -248,18 +269,18 @@ def __setData__(sql_con, sql_cur, uid:int, data_name:str, amount, sep=False):
     amount로 설정하는 함수
     '''
     if __dataCheck__(uid, data_name, amount, 'set'):
+        sql_con, sql_cur = __connectDB__()
         sql_cur.execute(f'UPDATE friendly_rate SET {data_name}=:amount WHERE uid=:uid;',{'uid':uid,'amount':amount})
         if sep:
             func = 'Set(내부 수동)'
         else:
             func = 'Set(내부 자동)'
         __logWrite__(uid,func,f'{data_name} ─→ {amount}')
-        __commit__(sql_con)
+        __commit__(sql_con, True)
         if sep:
-            __calcFriendlyRate__(sql_con, sql_cur, uid)
-            __commit__(sql_con)
+            __calcFriendlyRate__(uid)
 
-def __addData__(sql_con, sql_cur, uid:int, data_name:str, amount, sep=False):
+def __addData__(uid:int, data_name:str, amount, sep=False):
     '''
     friendly_rate 테이블에서 uid에 대한 data_name의 값을
     amount만큼 바꾸는 함수
@@ -267,7 +288,8 @@ def __addData__(sql_con, sql_cur, uid:int, data_name:str, amount, sep=False):
     기존 add~ 함수 어짜피 내부에서만 쓰이니까 전부 합쳐버림
     '''
     if __dataCheck__(uid, data_name, amount, 'add'):
-        sql_cur.execute(f'UPDATE friendly_rate SET {data_name}=(SELECT {data_name} FROM friendly_rate WHERE uid=:uid)+:amount WHERE uid=:uid;',{'uid':uid,'amount':amount})
+        sql_con, sql_cur = __connectDB__()
+        sql_cur.execute(f'UPDATE friendly_rate SET {data_name}={data_name}+:amount WHERE uid=:uid;',{'uid':uid,'amount':amount})
         if sep:
             func = 'Add(내부 수동)'
         else:
@@ -276,17 +298,19 @@ def __addData__(sql_con, sql_cur, uid:int, data_name:str, amount, sep=False):
             __logWrite__(uid,func,f'{data_name} + {amount}')
         else:
             __logWrite__(uid,func,f'{data_name} - {abs(amount)}')
-        __commit__(sql_con)
+        __commit__(sql_con,True)
         if sep:
-            __calcFriendlyRate__(sql_con, sql_cur, uid)
-            __commit__(sql_con)
+            __calcFriendlyRate__(uid)
+
+##############################################################
+##############################################################
+##### 더 이상 이 아래에서는 __connectDB__ 사용하지 말 것 #####
+##############################################################
+##############################################################
 
 def __getDataFromOutside__(uid:int, attribute:str):
     '''코드가 비슷한 것 같아서 그냥 4개를 전부 합쳐버림'''
-    sql_con, sql_cur = __connectDB__()
-    result = __getData__(sql_cur,uid,attribute,True)
-    __closeCon__(sql_con)
-    return result
+    return __getData__(uid,attribute,True)
 
 def getCommandCount(uid:int):
     return __getDataFromOutside__(uid, 'command_count')
@@ -307,7 +331,7 @@ def getRegisterDate(uid:int):
 def getFriendlyRate(uid:int):
     return Decimal(str(__getDataFromOutside__(uid, 'friendly_rate')))
 
-def __updateLastCallDate__(sql_con, sql_cur, uid:int, date:dt, sep=False):
+def __updateLastCallDate__(uid:int, date:dt, sep=False):
     '''
     마지막으로 부른 날짜를 현재 시간으로 바꾸고 마지막 호출 시간이 오늘이 아니면 day_count를 1 올리고 접속을 며칠만에 했는지에 따라 그에 따른 처리를 하는 함수
     return값은
@@ -318,9 +342,9 @@ def __updateLastCallDate__(sql_con, sql_cur, uid:int, date:dt, sep=False):
     #In [46]: dt.strptime('2023-05-01 12:34:56.789','%Y-%m-%d %H:%M:%S.%f')
     #Out[46]: datetime.datetime(2023, 5, 1, 12, 34, 56, 789000)
     __logWrite__(uid,'날짜 계산','해당 유저의 날짜계산 요청 접수')
-    last_call = dt.strptime(__getData__(sql_cur, uid, 'last_call'),'%Y-%m-%d %H:%M:%S.%f%z')
+    last_call = dt.strptime(__getData__(uid, 'last_call'),'%Y-%m-%d %H:%M:%S.%f%z')
     now = dt.now(tz(td(hours=9)))
-    __setData__(sql_con, sql_cur,uid,'last_call',now)
+    __setData__(uid,'last_call',now)
     if now.time() >= time(5,15):
         todayStart = dt(now.year, now.month, now.day, 5, 15, tzinfo=tz(td(hours=9)))
     else:
@@ -330,39 +354,33 @@ def __updateLastCallDate__(sql_con, sql_cur, uid:int, date:dt, sep=False):
         #In [64]: last_call = dt.strptime('2023-07-08 05:14:59.000','%Y-%m-%d %H:%M:%S.%f')
         #In [65]: last_call - todayStart
         #Out[65]: datetime.timedelta(days=-1, seconds=86399)
-        __addData__(sql_con, sql_cur, uid, 'day_count', 1)
+        __addData__(uid, 'day_count', 1)
         restDay = abs((last_call - todayStart).days)
         __logWrite__(uid,'날짜 계산',f'오늘 첫 사용, 미접속일 : {restDay}일')
-        if restDay > 2 and restDay <= 7:
-            gunba = __getData__(sql_cur,uid,'gunba')
-            if not gunba:
-                __addData__(sql_con, sql_cur,uid,'total_penalty',3)
-            else:
+        if restDay >= 15:
+            if gunba:
                 __logWrite__(uid,'commandCallCalc',f'해당 유저는 gunba가 True이므로 패널티를 부여하지 않았음')
-        else:
-            if gunba >= 2:
-                __logWrite__(uid,'commandCallCalc',f'해당 유저는 gunba가 2 이상이므로 패널티를 부여하지 않았음')
             else:
-                __addData__(sql_con, sql_cur,uid,'total_penalty',14+2*(restDay-8))
+                __addData__(uid,'total_penalty',sevenDayPenaltyInitial+sevenDayPenaltyPerDay*(restDay-15))
         returnArg = restDay
     else:
         returnArg = 0
     if sep:
-        __calcFriendlyRate__(sql_con, sql_cur, uid)
+        __calcFriendlyRate__(uid)
     return returnArg
         
 
-def __calcFriendlyRate__(sql_con, sql_cur, uid:int):
+def __calcFriendlyRate__(uid:int):
     '''
     현재 기록된 command_count, day_count, total_penalty의 값을 기준으로 friendly_rate를 계산하고 올바른 값으로 갱신하는 함수
     return값은 변경된 friendly_rate
     '''
-    command_count = __getData__(sql_cur, uid, 'command_count')
-    day_count = __getData__(sql_cur, uid, 'day_count')
-    total_penalty = Decimal(str(__getData__(sql_cur, uid, 'total_penalty')))
+    command_count = __getData__(uid, 'command_count')
+    day_count = __getData__(uid, 'day_count')
+    total_penalty = Decimal(str(__getData__(uid, 'total_penalty')))
     friendly_rate = command_count * commandPoint + day_count * dayPoint - total_penalty
     __logWrite__(uid, '친밀도 계산', f'friendly_rate = {friendly_rate}')
-    __setData__(sql_con, sql_cur, uid, 'friendly_rate', float(friendly_rate))
+    __setData__(uid, 'friendly_rate', float(friendly_rate))
     return friendly_rate
 
 def commandCallCalc(uid:int, date:dt):
@@ -378,11 +396,9 @@ def commandCallCalc(uid:int, date:dt):
     lastCallArg : __updateLastCallDate__의 주석 참고
     '''
     __logWrite__(uid,'commandCallCalc','해당 유저의 commandCallCalc 요청 접수')
-    sql_con, sql_cur = __connectDB__()
-    __addData__(sql_con, sql_cur, uid, 'command_count', 1)
-    lastCallArg = __updateLastCallDate__(sql_con, sql_cur, uid, date)
-    friendlyRateArg = __calcFriendlyRate__(sql_con, sql_cur, uid)
-    __commit__(sql_con,True)
+    __addData__(uid, 'command_count', 1)
+    lastCallArg = __updateLastCallDate__(uid, date)
+    friendlyRateArg = __calcFriendlyRate__(uid)
     __logWrite__(uid,'commandCallCalc',f'해당 유저의 commandCallCalc 요청 처리 완료 | lastCallArg는 {lastCallArg}')
     return friendlyRateArg, lastCallArg
 
@@ -401,6 +417,7 @@ if __name__ == '__main__':
     print('│ 4.           테이블 생성 │')
     print('│ 5.             유저 등록 │')
     print('│ 6.      친밀도 수동 계산 │')
+    print('│ 7. 전체 친밀도 수동 계산 │')
     print('│ (9).        치르노(바보) │')
     print('└──────────────────────────┘')
     arg = int(input('번호 입력 : '))
@@ -408,15 +425,13 @@ if __name__ == '__main__':
         print('BPM 999의 산수교실을 즐겨봐요!')
     elif arg == 1:
         uid = input('조회할 유저의 uid를 입력해주세요. 만약 모든 유저의 데이터를 조회하시려면 그냥 엔터를 쳐 주세요. : ')
-        sql_con, sql_cur = __connectDB__()
         try:
             uid = int(uid)
         except:
-            sql_cur.execute('SELECT * FROM friendly_rate;')
+            sql_data = __getData__(None,'*',True)
         else:
-            sql_cur.execute('SELECT * FROM friendly_rate where uid=:uid;',{'uid':uid})
+            sql_data = __getData__(uid,'*',True)
         finally:
-            sql_data = sql_cur.fetchall()
             print(f'데이터 개수 : {len(sql_data)}')
             print(tui.fixedWidth('uid',20,1)+tui.fixedWidth('최초 등록 시간',35,1)+tui.fixedWidth('마지막 호출 시간',35,1)+'군바'+tui.fixedWidth('command',9,2),tui.fixedWidth('day',9,2),tui.fixedWidth('penalty',12,2),tui.fixedWidth('호감도',12,2))
             for row in sql_data:
@@ -432,9 +447,7 @@ if __name__ == '__main__':
             pass
         else:
             amount = int(amount)
-        sql_con, sql_cur = __connectDB__()
-        __setData__(sql_con, sql_cur, uid, data_name, amount, True)
-        __closeCon__(sql_con)
+        __setData__(uid, data_name, amount, True)
         print('설정 작업이 완료되었습니다. 데이터가 반영되었는지는 조회 메뉴에서 조회해주세요.')
     elif arg == 3:
         uid = int(input('변경할 유저의 uid를 입력해주세요. : '))
@@ -446,9 +459,7 @@ if __name__ == '__main__':
             pass
         else:
             amount = int(amount)
-        sql_con, sql_cur = __connectDB__()
-        __setData__(sql_con, sql_cur, uid, data_name, amount, True)
-        __closeCon__(sql_con)
+        __setData__(uid, data_name, amount, True)
         print('변경 작업이 완료되었습니다. 데이터가 반영되었는지는 조회 메뉴에서 조회해주세요.')
     elif arg == 4:
         sql_con, sql_cur = __connectDB__()
@@ -465,6 +476,10 @@ if __name__ == '__main__':
             print(f'{uid} 유저의 데이터를 등록하는 중 오류가 발생했습니다.')
     elif arg == 6:
         uid = int(input('계산할 유저의 uid를 입력해주세요. : '))
-        sql_con, sql_cur = __connectDB__()
-        friendly_rate = __calcFriendlyRate__(sql_con, sql_cur, uid)
+        friendly_rate = __calcFriendlyRate__(uid)
         print(f'{uid} 유저의 친밀도가 다시 계산되어, {friendly_rate}로 바뀌었습니다.')
+    elif arg == 7:
+        sql_data = __getData__(None, 'uid')
+        for i in range(len(sql_data)):
+            __calcFriendlyRate__(sql_data[i])
+        print(f'전체 유저의 친밀도 계산이 완료되었습니다.')
